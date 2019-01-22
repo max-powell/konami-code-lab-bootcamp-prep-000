dece9b76a7d38decefff4a67fb85d2829210e27f
Max Powell <max_f_powell@hotmail.co.uk>
2019-01-22 14:37:38 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -12,21 +12,19 @@ const codes = [
 ];
 
 function onKeyDownHandler(e) {
-
+  const key = e.key;
+  let i = 0
+  if (key === codes[i]) {
+    i++
+    if (i === codes.length) {
+      alert('Congratulations!')
+    }
+  } else {
+    i = 0
+  }
 }
 
 function init() {
   var body = document.querySelector('body')
-  body.addEventListener('keydown', function(e) {
-    const key = e.key
-    let i = 0
-    if (key === codes[i]) {
-      i++
-      if (i === codes.length) {
-        alert('Conratulations!')
-      }
-    } else {
-      index = 0
-    }
-  })
+  body.addEventListener('keydown', onKeyDownHandler)
 }
